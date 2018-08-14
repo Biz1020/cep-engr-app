2d8eb0c06db60e66ecb5f6a74eed9594b30fc1e4
Elizabeth Faulkner <elizabethfaulkner@EKFMacbook.local>
2018-08-14 07:56:16 -0700

    I hope this works

--- src/init.js
+++ src/init.js
@@ -1,4 +1,30 @@
 // create a JavaScript object here with the following fields: firstName, lastName, jobTitle, homeOffice
-
+var button = {
+  firstName: "Elizabeth",
+  lastName: "Faulkner",
+  jobTitle: "Sr. Tax Business Analyst",
+  homeOffice: "Domain",
+  aboutMe: "I've wanted to transition into coding for so long, but never really knew how to make it happen.  Until now.  This program is amazing!"
+};
 
 // using jQuery and the object above, display the information as the appropriate button is clicked
+$(document).ready(function() {
+
+  $("#firstName").click(function(){
+   $("#textboxDiv").text(button.firstName);
+ });
+ $("#lastName").click(function(){
+   $("#textboxDiv").text(button.lastName);
+ });
+ $("#jobTitle").click(function(){
+   $("#textboxDiv").text(button.jobTitle);
+ });
+ $("#homeOffice").click(function(){
+   $("#textboxDiv").text(button.homeOffice);
+ });
+ $("#aboutMe").click(function(){
+  $("#textboxDiv").text(button.aboutMe);
+ });
+
+
+});
